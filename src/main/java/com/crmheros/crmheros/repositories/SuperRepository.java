d4d69e6ea0b8890f88ff3be0888a4d18688a31e4
Glenn Oberlé <oberle.glenn@icloud.com>
2021-03-04 10:16:09 +0100

    test

--- src/main/java/com/crmheros/crmheros/repositories/SuperRepository.java
+++ src/main/java/com/crmheros/crmheros/repositories/SuperRepository.java
@@ -1,2 +1,9 @@
-package com.crmheros.crmheros.repositories;public interface SuperRepository {
+package com.crmheros.crmheros.repositories;
+
+import com.crmheros.crmheros.models.Super;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface SuperRepository extends CrudRepository<Super, UUID> {
 }
