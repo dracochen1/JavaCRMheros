324bfaf0fd64ffa44022129d1db255dda0356fce
Yasmine <yasmine.bouslimani@gmail.com>
2021-03-10 19:14:05 +0100

    Test Rapport CRUD

--- src/test/java/com/crmheros/crmheros/TestBase.java
+++ src/test/java/com/crmheros/crmheros/TestBase.java
@@ -31,5 +31,8 @@ public abstract class TestBase {
     protected SatisfactionRepository satisfactionRepository;
 
     @Autowired
+    protected RapportRepository rapportRepository;
+
+    @Autowired
     protected MockMvc mockMvc;
 }
